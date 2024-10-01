da2c872277de345a9e61c0602b7855bf8d7c7c1a
Sean Dustin Verano <verano.seandustin@gmail.com>
2024-10-01 15:34:58 +0800

    Add components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,16 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomePage from "./HomePage";
+import TemplatesPage from "./TemplatesPage";
+
 function App() {
-  return <></>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/templates" element={<TemplatesPage />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
